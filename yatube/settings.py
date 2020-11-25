156aa1de3f292e3e18ccda753c7e3c56a131c9e2
Olga <60407728+gooncharova@users.noreply.github.com>
2020-11-25 15:34:18 +0300

    Delete secret key from settings

--- yatube/settings.py
+++ yatube/settings.py
@@ -7,9 +7,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "1^gtp6277_guj*^94=fhqx!t4khoin*cf#+wo+#t^%4=cphw@5"
-
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
 
